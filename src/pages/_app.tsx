174c6b798e2e77aa0477b14bef4667bde452e8e1
Kazuki Tobita <kazukiti201@gmail.com>
2021-05-11 15:53:01 +0900

    Modify settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
