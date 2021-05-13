691b7e14c3f1c38fb3049c8b30a1a9969636fc21
Kazuki Tobita <kazukiti201@gmail.com>
2021-05-13 14:07:39 +0900

    Fix bug

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/uyupun/official/proxy/config"
+	"github.com/uyupun/official/proxy/cmd/config"
 )
 
 func main() {
 	engine := gin.Default()
 
 	config.Init()
-	conf := configs.Get()
+	conf := config.Get()
 
 	engine.Run(conf.Listen)
 }
