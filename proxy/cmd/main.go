8b652143427e51bad5a2528026e4e0c887396fa6
Kazuki Tobita <kazukiti201@gmail.com>
2021-05-14 10:59:39 +0900

    Add gqlgen

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gin-gonic/gin"
 
 	"github.com/uyupun/official/proxy/cmd/config"
+	"github.com/uyupun/official/proxy/graph"
+	"github.com/uyupun/official/proxy/graph/generated"
 )
 
 func main() {
@@ -12,5 +16,27 @@ func main() {
 	config.Init()
 	conf := config.Get()
 
+	registerRoutes(engine)
 	engine.Run(conf.Listen)
+}
+
+func registerRoutes(engine *gin.Engine) {
+	engine.POST("/query", graphqlHandler())
+	engine.GET("/", playgroundHandler())
+}
+
+func graphqlHandler() gin.HandlerFunc {
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(ctx *gin.Context) {
+		h.ServeHTTP(ctx.Writer, ctx.Request)
+	}
+}
+
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(ctx *gin.Context) {
+		h.ServeHTTP(ctx.Writer, ctx.Request)
+	}
 }
--- proxy/go.mod
+++ proxy/go.mod
@@ -3,6 +3,8 @@ module github.com/uyupun/official/proxy
 go 1.16
 
 require (
+	github.com/99designs/gqlgen v0.13.0 // indirect
 	github.com/gin-gonic/gin v1.7.1
+	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
