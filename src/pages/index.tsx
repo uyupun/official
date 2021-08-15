758c38bc7314c00c7bf4fdbc847b3736d3aafbbc
Kazuki Tobita <kazukiti201@gmail.com>
2021-08-16 07:25:08 +0900

    Delete backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,6 @@
-import {client, query, useQuery} from '../lib/apollo';
-
-const TEST = query`
-query {
-  hoge {
-    id
-    name
-  }
-}
-`
-
 const Index = () => {
-
-  const {loading, error, data} = useQuery(TEST, {client});
-
   return (
-    <div>
-      <div>hoge</div>
-      {!loading && data.hoge[0].name}
-      {!loading && data.hoge[0].id}
-    </div>
+    <h1>official</h1>
   )
 };
 
