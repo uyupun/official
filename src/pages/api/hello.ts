032b65aa7643b5973e63bdd09fddc28fedc71551
takashi0602 <t.takashi0602@gmail.com>
2021-12-04 01:10:28 +0900

    refactor: warningの場合でもエラーとするようにオプションを追加する

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+const hello = (_: any, res: any) => {
   res.status(200).json({ name: 'John Doe' });
 };
+
+export default hello;
