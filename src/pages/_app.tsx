d944a5ce7035b98862a37bd235fa2ce604377909
takashi0602 <t.takashi0602@gmail.com>
2022-01-15 00:07:45 +0900

    feat: modern-css-resetをグローバルに適応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.css';
+import 'modern-css-reset/dist/reset.min.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
