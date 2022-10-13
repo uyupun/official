b70b50c54816466379d3aba4e3e866de8f72f34e
takashi0602 <t.takashi0602@gmail.com>
2022-10-13 23:58:50 +0900

    feat: テーマカラーの追加

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  white: '#FEFEFE',
+  black: '#333333',
+  silver: '#F8F8F8',
+  gray: '#C8C8C8',
+  blue: '#1E1ADE',
+  lightBlue: '#0080FF',
+  purple: '#B01CF6',
+} as const;
