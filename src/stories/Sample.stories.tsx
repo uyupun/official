69bfdab65d9857fb0b351cf82e919fff9193f45d
takashi0602 <t.takashi0602@gmail.com>
2022-10-15 01:09:16 +0900

    feat: サンプルコンポーネントでアイコンを使用する

--- src/stories/Sample.stories.tsx
+++ src/stories/Sample.stories.tsx
@@ -1,9 +1,13 @@
+import StarIcon from '@mui/icons-material/Star';
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 
 import { styles } from './sample.css';
 
 const SampleComponent = (props: { title: string }) => (
-  <h1 className={styles.title}>{props.title}</h1>
+  <h1 className={styles.title}>
+    <StarIcon sx={{ fontSize: 40 }} />
+    {props.title}
+  </h1>
 );
 
 export default {
--- src/stories/sample.css.ts
+++ src/stories/sample.css.ts
@@ -3,5 +3,9 @@ import { style } from '@vanilla-extract/css';
 import { theme } from '../styles/theme';
 
 export const styles = {
-  title: style({ color: theme.blue }),
+  title: style({
+    display: 'flex',
+    alignItems: 'center',
+    color: theme.blue,
+  }),
 };
