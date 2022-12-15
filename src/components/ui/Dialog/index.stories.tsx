b973481aac1e9e085ea3aa0965af82aae261ff05
takashi0602 <t.takashi0602@gmail.com>
2022-12-15 23:59:22 +0900

    feat: storybookにDialogコンポーネントを表示

---
+++ src/components/ui/Dialog/index.stories.tsx
@@ -0,0 +1,23 @@
+import { action } from '@storybook/addon-actions';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+
+import { Dialog } from './index';
+
+export default {
+  title: 'ui/Dialog',
+  component: Dialog,
+  parameters: {
+    docs: {
+      inlineStories: false,
+      iframeHeight: 500,
+    },
+  },
+} as ComponentMeta<typeof Dialog>;
+
+export const Index: ComponentStoryObj<typeof Dialog> = {
+  args: {
+    isOpen: false,
+    onClose: action('onClose'),
+    children: <div style={{ height: '1000px' }}>コンテンツ</div>,
+  },
+};
