abe8a7e345df66834593be95e641a2872ff985ed
takashi0602 <t.takashi0602@gmail.com>
2023-01-07 19:10:51 +0900

    ✨ feat: Headingコンポーネントの作成

---
+++ src/components/ui/Heading/index.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, Story } from '@storybook/react';
+
+import { Heading } from '.';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+} as ComponentMeta<typeof Heading>;
+
+export const _Heading: Story = () => {
+  return (
+    <div>
+      <Heading tag="h1">Heading level 1</Heading>
+      <Heading tag="h2">Heading level 2</Heading>
+    </div>
+  );
+};
---
+++ src/components/ui/Heading/index.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+import { styles } from './styles.css';
+
+export type HeadingProps = {
+  /**
+   * 見出しのタグ
+   */
+  tag: 'h1' | 'h2';
+  /**
+   * 見出しのコンテンツ
+   */
+  children: ReactNode;
+};
+
+const Heading = ({ tag = 'h1', children }: HeadingProps) => {
+  switch (tag) {
+    case 'h1':
+      return <h1 className={clsx(styles.common, styles.h1)}>{children}</h1>;
+    case 'h2':
+      return <h2 className={clsx(styles.common, styles.h2)}>{children}</h2>;
+  }
+};
+
+export { Heading };
