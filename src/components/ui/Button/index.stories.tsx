450cb770193d58b2407734728eb7d238ff1ca243
takashi0602 <t.takashi0602@gmail.com>
2023-01-14 00:52:43 +0900

    ♻️ refactor: Storybook上でのButtonコンポーネントの配置を修正

--- src/components/ui/Button/index.stories.tsx
+++ src/components/ui/Button/index.stories.tsx
@@ -12,20 +12,20 @@ export default {
 export const _Button: Story = () => {
   return (
     <dl>
-      <dt>Default</dt>
-      <dd style={{ marginBottom: '16px' }}>
+      <dt style={{ marginBottom: '8px' }}>Default</dt>
+      <dd style={{ marginBottom: '24px' }}>
         <Button type="button" onClick={action('clicked')}>
           Default Button
         </Button>
       </dd>
-      <dt>Disabled</dt>
-      <dd style={{ marginBottom: '16px' }}>
+      <dt style={{ marginBottom: '8px' }}>Disabled</dt>
+      <dd style={{ marginBottom: '24px' }}>
         <Button type="button" disabled onClick={action('clicked')}>
           Disabled Button
         </Button>
       </dd>
-      <dt>Circle</dt>
-      <dd style={{ marginBottom: '16px' }}>
+      <dt style={{ marginBottom: '8px' }}>Circle</dt>
+      <dd style={{ marginBottom: '24px' }}>
         <Button type="button" variant="circle" onClick={action('clicked')}>
           {/* TODO: アイコンコンポーネントに置き換える */}
           <Close sx={{ fontSize: 24 }} />
