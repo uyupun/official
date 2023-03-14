64483e5f6191ffb98e2d6a1634e4e1dead81e0a3
takashi0602 <t.takashi0602@gmail.com>
2023-03-14 23:17:00 +0900

    🎨 chore: ReactのStrict Modeに対応させる

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     dirs: ['src', 'public', '.storybook'],
   },
