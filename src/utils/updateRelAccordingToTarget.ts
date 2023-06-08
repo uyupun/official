015b1613321262569324767af5e85c96b422fe46
takashi0602 <t.takashi0602@gmail.com>
2023-06-08 23:47:08 +0900

    ♻️ refactor: 変数名の修正

--- src/utils/updateRelAccordingToTarget.ts
+++ src/utils/updateRelAccordingToTarget.ts
@@ -4,7 +4,7 @@ export const updateRelAccordingToTarget = (rel?: string, target?: HTMLAttributeA
   if (target !== '_blank') return rel;
 
   const requiredValues = ['noreferrer', 'noopener'];
-  const filteredRequiredValues = requiredValues.filter((value) => !rel?.includes(value));
-  if (filteredRequiredValues.length === 0) return rel;
-  return `${rel ?? ''} ${filteredRequiredValues.join(' ')}`.trim();
+  const missingRequiredValues = requiredValues.filter((value) => !rel?.includes(value));
+  if (missingRequiredValues.length === 0) return rel;
+  return `${rel ?? ''} ${missingRequiredValues.join(' ')}`.trim();
 };
