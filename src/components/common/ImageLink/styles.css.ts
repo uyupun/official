1eb1623af53b841dc6fa5add79ed3c692c22d6b2
takashi0602 <t.takashi0602@gmail.com>
2023-06-08 23:21:17 +0900

    ✨ feat: アニメーションの追加

--- src/components/common/ImageLink/styles.css.ts
+++ src/components/common/ImageLink/styles.css.ts
@@ -47,6 +47,7 @@ const styles = {
       width: '100%',
       height: '100%',
       opacity: 0,
+      transition: 'opacity 300ms',
       selectors: {
         [`${imageLink}:focus-visible &`]: {
           opacity: 1,
