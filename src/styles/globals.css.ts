3115f5e0c7debe817565149c1c71a72c865c84a5
takashi0602 <t.takashi0602@gmail.com>
2023-06-16 23:00:44 +0900

    ⏪️ revert: フォントのパスを戻す

--- src/styles/globals.css.ts
+++ src/styles/globals.css.ts
@@ -3,25 +3,25 @@ import { globalFontFace, globalStyle } from '@vanilla-extract/css';
 const font = 'RictyDiminished';
 
 globalFontFace(font, {
-  src: `url('./fonts/RictyDiminished-Regular.ttf') format('truetype')`,
+  src: `url('/fonts/RictyDiminished-Regular.ttf') format('truetype')`,
   fontWeight: 'normal',
   fontStyle: 'normal',
 });
 
 globalFontFace(font, {
-  src: `url('./fonts/RictyDiminished-Bold.ttf') format('truetype')`,
+  src: `url('/fonts/RictyDiminished-Bold.ttf') format('truetype')`,
   fontWeight: 'bold',
   fontStyle: 'normal',
 });
 
 globalFontFace(font, {
-  src: `url('$./fonts/RictyDiminished-Oblique.ttf') format('truetype')`,
+  src: `url('/fonts/RictyDiminished-Oblique.ttf') format('truetype')`,
   fontWeight: 'normal',
   fontStyle: 'oblique',
 });
 
 globalFontFace(font, {
-  src: `url('./fonts/RictyDiminished-BoldOblique.ttf') format('truetype')`,
+  src: `url('/fonts/RictyDiminished-BoldOblique.ttf') format('truetype')`,
   fontWeight: 'bold',
   fontStyle: 'oblique',
 });
