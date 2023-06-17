6066a9821c6d1c4cd38de8311778d7360f64f3e9
takashi0602 <t.takashi0602@gmail.com>
2023-06-17 23:19:24 +0900

    ✨ feat: lang属性の追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
