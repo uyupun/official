57f0459b33991836889b69c6b29ad4bb84e808b7
takashi0602 <t.takashi0602@gmail.com>
2023-06-18 00:05:31 +0900

    ♻️ refactor: ビューポートの設定を削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
