3adeaf5b34d9a9720faf21975c148ecddfaf6260
Kazuki Tobita <kazukiti201@gmail.com>
2023-06-22 00:34:28 +0900

    ✨ feat: Twitterカードの設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,20 @@ export default function Document() {
     <Html lang="ja">
       <Head>
         <meta name="description" content={process.env.NEXT_PUBLIC_DESC} />
+
         <meta property="og:title" content={process.env.NEXT_PUBLIC_TITLE} />
         <meta property="og:description" content={process.env.NEXT_PUBLIC_DESC} />
         <meta property="og:image" content="https://i.imgur.com/cmu4u1h.png" />
         <meta property="og:url" content="https://uyupun.tech" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ja_JP" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={process.env.NEXT_PUBLIC_TITLE} />
+        <meta name="twitter:description" content={process.env.NEXT_PUBLIC_DESC} />
+        <meta name="twitter:site" content="@uyupunpopunpo" />
+        <meta name="twitter:image" content="https://i.imgur.com/cmu4u1h.png" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
