a97e5ee488c26a0de22d0134c95e05fc1b79644b
Kazuki Tobita <kazukiti201@gmail.com>
2023-06-22 00:22:29 +0900

    ✨ feat: 基本的なOGPタグを設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
     <Html lang="ja">
       <Head>
         <meta name="description" content={process.env.NEXT_PUBLIC_DESC} />
+        <meta property="og:title" content={process.env.NEXT_PUBLIC_TITLE} />
+        <meta property="og:description" content={process.env.NEXT_PUBLIC_DESC} />
+        <meta property="og:image" content="https://i.imgur.com/cmu4u1h.png" />
+        <meta property="og:url" content="https://uyupun.tech" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="ja_JP" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
