ba6230ee8f43be7386810249d2ee2e3b49c17b48
Kazuki Tobita <kazukiti201@gmail.com>
2023-06-21 17:47:01 +0900

    ✨ feat: descriptionの設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <meta
+          name="description"
+          content="(´･ェ･｀)「あ、もしもし？ぅゅだけど…うん、そうぷんぽぷんぽ」"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
