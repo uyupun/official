890157812ab3b7b8deadca9f503e64be8f186493
Kazuki Tobita <kazukiti201@gmail.com>
2023-06-22 00:41:08 +0900

    💄 style: OGPの画像を変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
 
         <meta property="og:title" content={process.env.NEXT_PUBLIC_TITLE} />
         <meta property="og:description" content={process.env.NEXT_PUBLIC_DESC} />
-        <meta property="og:image" content="https://i.imgur.com/cmu4u1h.png" />
+        <meta property="og:image" content="https://i.imgur.com/8a6Rp4N.png" />
         <meta property="og:url" content="https://uyupun.tech" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ja_JP" />
@@ -17,7 +17,7 @@ export default function Document() {
         <meta name="twitter:title" content={process.env.NEXT_PUBLIC_TITLE} />
         <meta name="twitter:description" content={process.env.NEXT_PUBLIC_DESC} />
         <meta name="twitter:site" content="@uyupunpopunpo" />
-        <meta name="twitter:image" content="https://i.imgur.com/cmu4u1h.png" />
+        <meta name="twitter:image" content="https://i.imgur.com/8a6Rp4N.png" />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
