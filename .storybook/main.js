3f70f59600c15a0686df3beeb6fa1e77b6e9cf8a
takashi0602 <t.takashi0602@gmail.com>
2023-07-13 22:59:19 +0900

    🎨 chore: Storybookで.css.tsを読み込めるようにする

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+const { merge } = require('webpack-merge');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
@@ -8,4 +11,11 @@ module.exports = {
   docs: {
     autodocs: true,
   },
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) =>
+    merge(config, {
+      plugins: [new VanillaExtractPlugin()],
+    }),
 };
