6e303e69ee10bcf154a2ca5fdee7eb81b45c6436
takashi0602 <t.takashi0602@gmail.com>
2023-07-29 01:12:21 +0900

    ✨ feat: Cicaフォントを適用する

--- src/styles/globals.css.ts
+++ src/styles/globals.css.ts
@@ -1,27 +1,27 @@
 import { globalFontFace, globalStyle } from '@vanilla-extract/css';
 
-const font = 'RictyDiminished';
+const font = 'Cica';
 
 globalFontFace(font, {
-  src: `url('/fonts/RictyDiminished-Regular.ttf') format('truetype')`,
+  src: `url('/fonts/Cica-Regular.ttf') format('truetype')`,
   fontWeight: 'normal',
   fontStyle: 'normal',
 });
 
 globalFontFace(font, {
-  src: `url('/fonts/RictyDiminished-Bold.ttf') format('truetype')`,
+  src: `url('/fonts/Cica-Bold.ttf') format('truetype')`,
   fontWeight: 'bold',
   fontStyle: 'normal',
 });
 
 globalFontFace(font, {
-  src: `url('/fonts/RictyDiminished-Oblique.ttf') format('truetype')`,
+  src: `url('/fonts/Cica-Oblique.ttf') format('truetype')`,
   fontWeight: 'normal',
   fontStyle: 'oblique',
 });
 
 globalFontFace(font, {
-  src: `url('/fonts/RictyDiminished-BoldOblique.ttf') format('truetype')`,
+  src: `url('/fonts/Cica-BoldOblique.ttf') format('truetype')`,
   fontWeight: 'bold',
   fontStyle: 'oblique',
 });
