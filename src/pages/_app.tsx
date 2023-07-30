c8bf293c1feb6cee8dada5ce837625d5ec80ae77
takashi0602 <t.takashi0602@gmail.com>
2023-07-31 01:05:38 +0900

    ✨ feat: next/fontを使用してフォントを読み込む

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import 'modern-css-reset/dist/reset.min.css';
 import Head from 'next/head';
 
+import { cica } from '@/styles/font';
+
 import type { AppProps } from 'next/app';
 import type { FC } from 'react';
-import '../styles/globals.css';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -11,7 +12,9 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
       </Head>
-      <Component {...pageProps} />
+      <div className={cica.className}>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 };
