fc016930e3bed30b366e647cd6e9fd539a42bd9b
takashi0602 <t.takashi0602@gmail.com>
2023-10-16 01:52:16 +0900

    ✨ feat: DynamicとSprinklesを組み合わせることができないので、style関数を使用してレスポンシブ対応を行える関数を作成

--- src/styles/breakpoint.ts
+++ src/styles/breakpoint.ts
@@ -1,1 +1,12 @@
+import { style, type StyleRule } from '@vanilla-extract/css';
+
 export const breakpoint = 'screen and (min-width: 768px)';
+
+export const breakpointStyle = (mobileStyle: StyleRule, desktopStyle: StyleRule): string => {
+  return style({
+    ...mobileStyle,
+    '@media': {
+      'screen and (min-width: 768px)': desktopStyle,
+    },
+  });
+};
