3f89873a284eb0beadf12745e6678ef7f5850ab9
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2024-03-23 18:19:12 +0900

    ✨ feat: HeadingコンポーネントでComponentPropsWithoutRefを使用する

--- src/components/base/Heading/index.tsx
+++ src/components/base/Heading/index.tsx
@@ -2,25 +2,33 @@ import clsx from 'clsx';
 
 import { styles } from './styles.css';
 
-import type { FC, ReactNode } from 'react';
+import type { ComponentPropsWithoutRef, FC, ReactNode } from 'react';
 
-type Props = {
+type Props<T extends 'h1' | 'h2'> = {
   /**
    * 見出しのタグ
    */
-  tag?: 'h1' | 'h2';
+  tag?: T;
   /**
    * 見出しのコンテンツ
    */
   children: ReactNode;
-};
+} & Omit<ComponentPropsWithoutRef<T>, 'tag' | 'children'>;
 
-const Heading: FC<Props> = ({ tag = 'h1', children }) => {
+const Heading: FC<Props<'h1' | 'h2'>> = ({ tag = 'h1', children, className, ...rest }) => {
   switch (tag) {
     case 'h1':
-      return <h1 className={clsx(styles.common, styles.h1)}>{children}</h1>;
+      return (
+        <h1 className={clsx(styles.common, styles.h1, className)} {...rest}>
+          {children}
+        </h1>
+      );
     case 'h2':
-      return <h2 className={clsx(styles.common, styles.h2)}>{children}</h2>;
+      return (
+        <h2 className={clsx(styles.common, styles.h2, className)} {...rest}>
+          {children}
+        </h2>
+      );
   }
 };
 
