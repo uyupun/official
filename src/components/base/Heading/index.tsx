523ac3c87b4ffde0e2530b7b56f4a9153a5cd89b
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2024-03-23 18:31:14 +0900

    ♻️ refactor: switch文の削除

--- src/components/base/Heading/index.tsx
+++ src/components/base/Heading/index.tsx
@@ -16,20 +16,12 @@ type Props<T extends 'h1' | 'h2'> = {
 } & Omit<ComponentPropsWithoutRef<T>, 'tag' | 'children'>;
 
 const Heading: FC<Props<'h1' | 'h2'>> = ({ tag = 'h1', children, className, ...rest }) => {
-  switch (tag) {
-    case 'h1':
-      return (
-        <h1 className={clsx(styles.common, styles.h1, className)} {...rest}>
-          {children}
-        </h1>
-      );
-    case 'h2':
-      return (
-        <h2 className={clsx(styles.common, styles.h2, className)} {...rest}>
-          {children}
-        </h2>
-      );
-  }
+  const Component = tag;
+  return (
+    <Component className={clsx(styles.common, styles[tag], className)} {...rest}>
+      {children}
+    </Component>
+  );
 };
 
 export { Heading };
